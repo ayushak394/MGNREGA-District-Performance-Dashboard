95fb679c07c216fd2c4f8cd769b45b46179a8baf
“Ayushak394” <ayush.ak394@gmail.com>
2025-11-29 21:32:15 +0530

    Minor Error Fixed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE =
+export const API_BASE =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api";
 
 export const getDistricts = () => axios.get(`${API_BASE}/districts`);
